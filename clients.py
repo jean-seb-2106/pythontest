da8cdccc83c4eaa0f9ef1a7e5053e0851d543c8a
jean-seb-2106 <jean.sebban@insee.fr>
2023-01-06 10:30:30 +0000

    les comptes bancaires

--- clients.py
+++ clients.py
@@ -1,8 +1,19 @@
 from comptebancaire import Compte #bonne pratique
-import comptebancaire as cb
+#import comptebancaire as cb autre façon de faire
 
 client1 = Compte(titulaire = "Jean",solde = 150)
-client2 = cb.Compte(titulaire, solde)
+#client2 = cb.Compte(titulaire, solde) #correspond à l'autre façon de faire
 print(client1.titulaire)
 print(client1.solde)
 client1.affiche_solde()
+
+client1.depot(100)
+print(client1.solde)
+
+client1.retrait(100)
+print(client1.solde)
+
+client1.retrait(1000)
+client2 = Compte("René",1000)
+client1.transfert(50, client2)
+print(client2.solde)
--- comptebancaire.py
+++ comptebancaire.py
@@ -1,9 +1,25 @@
 class Compte:
 
-    def __init__(self,titulaire,solde):
+    def __init__(self, titulaire, solde):
         self.titulaire = titulaire
         self.solde = solde
     
     def affiche_solde(self):
-        print("Le solde du compte de " + str(self.titulaire) + " est de " + str(self.solde) + " €")
-
+        print("Le solde du compte de " + self.titulaire + " est de " + str(self.solde) + " €")
+    
+    def depot(self, montant):
+        self.solde = self.solde + montant
+    
+    def retrait(self, montant):
+        if montant <= self.solde:
+            print("Retrait accepté")
+            self.solde = self.solde - montant
+        if montant > self.solde:
+            print("Retrait refusé : fonds insuffisants.")
+    
+    def transfert(self, montant, destinataire):
+        if montant <= self.solde:
+            destinataire.solde = destinataire.solde + montant
+            print("Transfert effectué")
+        if montant > self.solde:
+            print("Transfert refusé : fonds insuffisants.")
