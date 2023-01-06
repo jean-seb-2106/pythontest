1b12def968e256745fa65f5a404f56ded3930774
jean-seb-2106 <jean.sebban@insee.fr>
2023-01-06 11:18:06 +0000

    suite du tp

--- testpokemon.py
+++ testpokemon.py
@@ -21,3 +21,10 @@ print(dresseur1.Nom)
 dresseur1.capture(perso1)
 dresseur1.capture(perso2)
 print(dresseur1.listepokemon[0].statut()) #afficher le statut du premier pokemon capturé par le dresseur 1
+
+def combat(perso1,perso2):
+    while(perso1.vie > 0 | perso2.vie > 0):
+        perso1.vie = perso1.vie - perso2.degats
+        perso2.vie = perso2.vie - perso1.degats
+    if perso1.vie > perso2.vie:
+        print(perso1.statut[0]+" a gagné")
